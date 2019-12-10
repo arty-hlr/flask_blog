25625f300837fdcbf0ff74a71d7607e918e21ae8
florian <flrn.pjd@protonmail.com>
2019-12-11 00:35:11 +0100

    modified session key implementation

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ ADMIN_HASH = credentials.admin_hash
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
-SECRET_KEY = os.urandom(12)
+SECRET_KEY = credentials.session_key
 SITE_WIDTH = 800
 
 
